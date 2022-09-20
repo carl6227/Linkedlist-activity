abe44b0610cd652e7d40744fe2f202600ab58c80
carl6227 <19104922@usc.edu.ph>
2022-09-20 16:22:22 +0800

    first commit

---
+++ class.js
@@ -0,0 +1,122 @@
+
+class Node{
+    constructor(data, next = null){
+        this.data = data,
+        this.next = next
+    }
+    
+}
+
+class LinkedList{
+    constructor(){
+        this.head = null;
+    }
+    push(value){
+        let node= new Node(value);
+        if(!this.head){
+            this.head=node;
+            return value;
+        }
+        let pointer=this.head;
+        while(pointer.next!==null){
+            pointer=pointer.next;
+        }
+        pointer.next=node;
+        return value;
+        
+    }
+    
+    shift(){
+        if (!this.head)return null
+        let currentHead =this.head
+        let previousHead = this.head;
+        this.head = currentHead.next;
+        return previousHead.value = null;  
+    }
+
+
+    traverse(){
+        if(!this.head)return[];
+        if(!this.head.next)return[this.head.data];
+        let collection=[]   
+        let pointer=this.head
+        while(pointer !== null){
+            collection.push(pointer.data)
+            pointer=pointer.next
+        }
+        return collection
+    }
+
+    pop(){
+        if (!this.head)return null
+        let pointer =this.head
+        let last_node=null
+
+        if (!this.head.next){
+            last_node=this.head
+            this.head=null
+            return last_node.data
+        }
+
+       
+        while (pointer.next !==null){
+            if(!pointer.next.next){
+                pointer=pointer
+                last_node = pointer.next
+                break
+            }else{
+                pointer =pointer.next
+            }
+        }
+
+        pointer.next = null
+        
+        return last_node.data
+    }
+    forEach(callback){
+        if(!this.head) return;
+        
+        let pointer = this.head;
+
+        while (pointer !== null){
+            callback(pointer.data);
+            pointer = pointer.next
+        }
+
+        return;
+    }
+   
+}
+
+
+
+
+let queueLinkedList = new LinkedList();
+let stackLinkedList = new LinkedList();
+
+queueLinkedList.push(1);
+queueLinkedList.push(2);
+queueLinkedList.push(3);
+queueLinkedList.push(4);
+queueLinkedList.push(5);
+queueLinkedList.push(6);
+console.log('original linkedList',queueLinkedList.traverse())
+queueLinkedList.shift();
+console.log('queueLInkedList',queueLinkedList.traverse());
+
+console.log("--------------------------------------")
+
+stackLinkedList.push(7);
+stackLinkedList.push(8);
+stackLinkedList.push(9);
+stackLinkedList.push(10);
+stackLinkedList.push(11);
+stackLinkedList.push(12);
+console.log('original linkedList',stackLinkedList.traverse())
+
+stackLinkedList.pop();
+console.log('stackLInkedList',stackLinkedList.traverse());
+
+
+const nullish = [] === null
+console.log(nullish)
---
+++ index.js
@@ -0,0 +1,84 @@
+// const list={
+//     head:null,
+//     push:function(value){
+//         const node = {
+//             value,next:null
+//         };
+//         if(!this.head){
+//             this.head=node;
+//             return value;
+//         }
+//         let pointer =this.head;
+//         while (pointer.next !==null){
+//             pointer =pointer.next;
+//         }
+//         pointer.next=node;
+//         return value;
+//     },
+
+    // insertIntheBegginning(node){
+    //     node.next = this.head;   
+    //     this.head = node;
+    //     return this.head;
+    // }
+//     traverse:function(){
+//         if(!this.head)return[];
+//         if(!this.head.next)return [this.head.value];
+//         let collection=[]
+//         let pointer=this.head
+//         while(pointer !== null){
+//             collection.push(pointer.value)
+//             pointer =pointer.next
+//         }
+//         return collection
+//     },
+//     pop:function (){
+//         if (!this.head)return null
+//         let pointer =this.head
+//         let last_node=null
+
+//         if (!this.head.next){
+//             last_node=this.head
+//             this.head=null
+//             return last_node.value
+//         }
+//         while (pointer.next !==null){
+//             if(!pointer.next.next){
+//                 pointer=pointer
+//                 last_node = pointer.next
+//                 break
+//             }else{
+//                 pointer =pointer.next
+//             }
+//         }
+
+//         pointer.next = null
+        
+//         return last_node.value
+//     },
+//     forEach: function(callback){
+//         if(!this.head) return;
+        
+//         let pointer = this.head;
+
+//         while (pointer !== null){
+//             callback(pointer.value);
+//             pointer = pointer.next
+//         }
+
+//         return;
+//     }
+
+// }
+
+// list.push(1);
+// list.push(2);
+// list.push(3);
+// list.push(4);
+// list.push(5);
+// list.push(6);
+// console.log(list)
+// console.log('Array 0',list.traverse());
+// list.pop();
+// list.pop();
+// console.log('Array 1',list.traverse())
